84759bfc23c6d3b3d450d264d304302d5730bd39
AmirHSoukhteh <ascr.amir@gmail.com>
2024-03-26 13:21:20 +0330

    now when you run code, computer say bitcoin price

--- src/project.py
+++ src/project.py
@@ -1,5 +1,11 @@
 import requests
 import json
+import speake3
+
+# Setup text-to-speach
+engine = speake3.Speake()
+engine.set('voice', 'en')
+engine.set('speed', '150')
 
 # Get api from coindesk.com 
 r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
@@ -12,5 +18,8 @@ btc_price = data['bpi']['USD']['rate']
 
 # Change type of btc_price to int
 btc_price = int(float(btc_price.replace(",","")))
-
 print(f"bitcoin price is {btc_price}")
+
+# Speak text
+engine.say(f"bitcoin price is {btc_price}")
+engine.talkback()
