81b904adb9d0b643ebf8d5c6b09219913b42f55c
AmirHSoukhteh <ascr.amir@gmail.com>
2024-03-26 12:34:31 +0330

    add comment

--- src/project.py
+++ src/project.py
@@ -1,8 +1,12 @@
 import requests
 import json
 
+# Get api from coindesk.com 
 r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
+# Change type of data from json to dictionary
 data = json.loads(r.text)
 
-print(data['bpi']['USD']['rate'])
+# use dictionary key-value to find price 
+btc_price = data['bpi']['USD']['rate']
+
